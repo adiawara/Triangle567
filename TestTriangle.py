333c082db685a87e00000d1e3b14051c3fe01b31
adiawara <adiawara@stevens.edu>
2021-10-29 22:49:41 -0400

    Add files via upload

--- TestTriangle.py
+++ TestTriangle.py
@@ -7,8 +7,10 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 @author: rk
 """
 
-import unittest
+import unittest         # calling on test case
 
+import Triangle
+from Triangle import *
 from Triangle import classifyTriangle
 
 # This code implements the unit test functionality
@@ -17,14 +19,68 @@ from Triangle import classifyTriangle
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
-    def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+    def testRightTriangleA(self):
+        try:
+            self.assertEqual(Triangle.classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+            self.assertEqual(Triangle.classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        except:
+            print("Test Failed")
 
-    def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+    #def testRightTriangleB(self): 
+        #self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
         
-    def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+    def testEquilateralTriangles(self):
+        try:
+            self.assertEqual(Triangle.classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        except:
+            print("Test Failed")
+
+    def testIsoscelesTriangles(self):
+        try:
+            self.assertEqual(Triangle.classifyTriangle(4,5,5),'Isosceles','4,5,5 should be isosceles')
+        except:
+            print("Test Failed")
+
+    def testScaleneTriangles(self):
+        try:
+            self.assertEqual(Triangle.classifyTriangle(7,8,9),'Scalene','7,8,9 should be scalene')
+        except:
+            print("Test Failed")
+
+    def testIllegalInput_1(self):
+        try:
+            self.assertEqual('InvalidInput', Triangle.classifyTriangle(-1,- 1, -1))
+        except:
+            print("Test Failed")
+
+    def testIllegalInput_2(self):
+        try:
+            self.assertEqual('InvalidInput', Triangle.classifyTriangle(0, 0, 0))
+        except:
+            print("Test Failed")
+
+    def testIllegalInput_3(self):
+        try:
+            self.assertEqual('InvalidInput', Triangle.classifyTriangle(8, 6, -10))
+        except:
+            print("Test Failed")
+    def testIllegalInput_4(self):
+        try:
+            self.assertEqual('InvalidInput', Triangle.classifyTriangle(1, 1, 0))
+        except:
+            print("Test Failed")
+
+    def testIllegalTriangle(self):
+        try:
+            self.assertEqual('NotATriangle', Triangle.classifyTriangle(9, 2, 5))
+        except:
+            print("Test Failed")
+
+    def testIllegalInput_5(self):
+        try:
+            self.assertEqual('InvalidInput', Triangle.classifyTriangle(m, n, o))
+        except:
+            print("Test Failed")
 
 if __name__ == '__main__':
     print('Running unit tests')
