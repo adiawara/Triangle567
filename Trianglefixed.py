12e5cb7c6545fd4926a8f1e49d5d2a2c3fea145f
adiawara <adiawara@stevens.edu>
2021-11-01 22:48:30 -0400

    Add files via upload

--- Trianglefixed.py
+++ Trianglefixed.py
@@ -29,18 +29,18 @@ def classifyTriangle(a,b,c):
 
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
-        #return 'InvalidInput'
-        print("InvalidInput")
+        return 'InvalidInput'
+        #print("InvalidInput")
         
     if a <= 0 or b <= 0 or c <= 0:          #5th error fixed
-        #return 'InvalidInput'
-        print("InvalidInput")
+        return 'InvalidInput'
+        #print("InvalidInput")
     
     # verify that all 3 inputs are integers  
     # Python's "isinstance(object,type) returns True if the object is of the specified type
     if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        #return 'InvalidInput';
-        print("InvalidInput")
+        return 'InvalidInput';
+        #print("InvalidInput")
       
     # This information was not in the requirements spec but 
     # is important for correctness
@@ -55,13 +55,14 @@ def classifyTriangle(a,b,c):
         return 'Equilateral'
         #print("Equilateral")
     elif ((a ** 2) + (b ** 2)) == (c ** 2) or ((a ** 2) + (c ** 2)) == (b ** 2) or ((c ** 2) + (b ** 2)) == (a ** 2):
-        print("Right")
-        #return 'Right'
+        #print("Right")
+        return 'Right'
     elif (a != b) and  (b != c) and (a != c):                                       # 4th error
-        #return 'Scalene'
-        print("Scalene")
+        return 'Scalene'
+        #print("Scalene")
     else:
-        return print("Isosceles") #'Isosceles'
+        return  'Isosceles'
+        #print("Isosceles")
 
 #classifyTriangle(3,4,5)
 #classifyTriangle(3,3,3)
